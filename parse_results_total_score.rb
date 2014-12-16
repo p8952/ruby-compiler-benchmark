be84c802bb0d406583ae278ee2c3e121af92cb3a
Peter Wilmott <p@p8952.info>
2014-12-16 19:53:48 +0000

    Total scores for each column

--- parse_results_total_score.rb
+++ parse_results_total_score.rb
@@ -2,8 +2,8 @@
 
 results_file = Dir.glob('ruby-benchmark-suite/results/rbs/*.csv').last
 exit if results_file.nil?
-results = File.read(results_file)
-results.lines.each_with_index do |line, index|
+raw_results = File.read(results_file)
+results = raw_results.lines.each_with_index.map do |line, index|
 	next if index == 0
 
 	unsorted_results = line.split(',').drop(2)
@@ -13,5 +13,14 @@ results.lines.each_with_index do |line, index|
 		unsorted_results[(unsorted_results.index(a))] = (sorted_results.length - i)
 	end
 
-	puts unsorted_results.join(',')
+	unsorted_results
+end
+results.compact!
+
+results.first.length.times do |i|
+	results_total = []
+	results.each do |result|
+		results_total << result[i]
+	end
+	puts results_total.map(&:to_i).reduce(:+)
 end
